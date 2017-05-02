7ff85503627b3bd20c3694c34b0bf36b2630a805
promethwill88 <wfong.bk@gmail.com>
2017-05-02 12:25:14 -0700

    first commit

---
+++ my-react-todo-pokemon/src/config/routes.js
@@ -0,0 +1,12 @@
+// src/config/routes.js
+import React from 'react'
+import {Route} from 'react-router'
+import App from '../App'
+import TodosContainer from '../containers/TodosContainer'
+
+
+module.exports = (
+	<Route path='/' component={App}>
+    	<Route path='/pokemons' component={TodosContainer}/>
+  	</Route>
+)
